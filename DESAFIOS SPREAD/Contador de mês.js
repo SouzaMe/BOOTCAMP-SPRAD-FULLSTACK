3f984e832b18c41592fe7ec0dc9d99143bbb4641
Marciano Souza <71178371+SouzaMe@users.noreply.github.com>
2022-06-12 10:36:56 -0300

    Add files via upload

---
+++ DESAFIOS SPREAD/Contador de mês.js
@@ -0,0 +1,24 @@
+// a função fica dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (saída) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não usar
+
+deixe  linhas  =  obtém ( ) . split ( '\n' ) ;
+
+deixe  mês  =  linhas . deslocamento ( ) ;
+
+deixe  meses  =  {
+  "1" : "Janeiro" ,
+  "2" : "Fevereiro" ,
+  "3" : "Março" ,
+  "4" : "Abril" ,
+  "5" : "Maio" ,
+  "6" : "Junho" ,
+  "7" : "Julho" ,
+  "8" : "Agosto" ,
+  "9" : "Setembro" ,
+  "10" : "Outubro" ,
+  "11" : "Novembro" ,
+  "12" : "Dezembro" ,
+} ;
+//TODO: Complete os espaços em branco com uma solução possível para o desafio
+
+imprimir ( meses [ mês ] ) ;
