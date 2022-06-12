31bbb84cde21e3b40439e583f6f8c5675cc9caf4
Marciano Souza <71178371+SouzaMe@users.noreply.github.com>
2022-06-12 10:37:56 -0300

    Add files via upload

---
+++ DESAFIOS SPREAD/Soma.js
@@ -0,0 +1,10 @@
+// a função fica dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (saída) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não usar
+
+deixe  A  =  parseInt ( get ( ) ) ;
+deixe  B  =  parseInt ( get ( ) ) ;
+
+//TODO: Complete os espaços em branco com uma solução possível para o desafio
+
+seja  SOMA  =  A + B           ;               
+print ( "SOMA=" +  SOMA  ) ;
